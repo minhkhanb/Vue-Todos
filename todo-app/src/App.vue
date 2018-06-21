23e5997634f403fdf106dd6e31adce9ae435f8ab
KhaLe <minhkhanb@gmail.com>
2018-06-21 14:01:31 +0700

    update component

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div id="app">
-    <div>
-      <Porfolio v-bind="users"></Porfolio>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Porfolio from './components/Porfolio/Porfolio'
 export default {
-  name: 'App',
-  components: {
-    Porfolio
-  },
-  data () {
-    return {
-      users: [{
-                  id: '1',
-                  email: 'khale@success.com',
-                  fullName: 'Kha Le',
-                  password: '123',
-                  avatar: 'avatar.png'
-                }],
-    };
-  }
+  name: 'App'
 }
 </script>
 <style src="./main.scss"></style>
--- todo-app/src/router/index.js
+++ todo-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import SignIn from '../components/SignIn/SignIn'
 
 Vue.use(Router);
 
@@ -8,9 +9,9 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
     }
   ]
 })
